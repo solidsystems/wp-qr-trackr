e2b596d84882936c75703c6b0ebdaa9f2a9afddf
Michael Erps <michaelerps@Michaels-MacBook-Pro.local>
2025-06-15 00:28:53 -0400

    fix: update linting dependencies and configurations

--- wp-content/plugins/wp-qr-trackr/.eslintrc.js
+++ wp-content/plugins/wp-qr-trackr/.eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
 	root: true,
 	extends: [
 		'plugin:@wordpress/eslint-plugin/recommended',
+		'plugin:@wordpress/eslint-plugin/jsdoc',
 	],
 	env: {
 		browser: true,
 		jquery: true,
+		es6: true,
 	},
 	globals: {
 		wp: true,
@@ -15,5 +17,12 @@ module.exports = {
 		'@wordpress/no-global-active-element': 'off',
 		'@wordpress/no-global-get-selection': 'off',
 		'@wordpress/no-unsafe-wp-apis': 'off',
+		'@wordpress/no-null': 'off',
+		'@wordpress/no-unsafe-return': 'off',
+	},
+	parserOptions: {
+		requireConfigFile: false,
+		ecmaVersion: 2022,
+		sourceType: 'module',
 	},
 }; 
