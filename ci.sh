984128b438e8b966d2c01e3aa5c562a2c49e7fd1
Michael Erps <michaelerps@Michaels-MacBook-Pro.local>
2025-06-15 00:23:33 -0400

    fix: update package dependencies and husky configuration

--- ci.sh
+++ ci.sh
@@ -10,8 +10,7 @@ echo "Installing Yarn dependencies..."
 yarn install
 
 echo "Setting up husky pre-commit hooks..."
-yarn add -D husky
-yarn husky install
+yarn prepare
 yarn husky add .husky/pre-commit "yarn lint && yarn stylelint && phpcs -d memory_limit=512M --standard=.phpcs.xml --ignore=vendor ."
 
 echo "Running PHP_CodeSniffer..."
