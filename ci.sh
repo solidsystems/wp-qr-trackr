419b39e082272853c2ad537c1ee22ce4aee3ba43
Michael Erps <michaelerps@Michaels-MacBook-Pro.local>
2025-06-22 14:04:20 -0400

    fix(ci): Finalize containerized CI workflow

--- ci.sh
+++ ci.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -e
 
+echo "--- Setting up WordPress Test Environment ---"
+# Move to the WordPress root
+cd /var/www/html
+
+# Copy the plugin source to the correct location
+cp -a /usr/src/app/wp-content/. ./wp-content/
+
+# Install the WordPress test suite
+/usr/src/app/wp-content/plugins/wp-qr-trackr/bin/install-wp-tests.sh \
+  wordpress_test \
+  rootpass \
+  db-nonprod \
+  db-nonprod \
+  latest
+
+# Return to the plugin directory to run tests
+cd /var/www/html/wp-content/plugins/wp-qr-trackr
+
 echo "--- Running PHP Code Sniffer ---"
 ./vendor/bin/phpcs --standard=.phpcs.xml
 
