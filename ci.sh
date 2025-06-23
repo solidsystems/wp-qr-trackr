7b56c67bc62c48c04b9237b7ebe4c26a67863ad0
Michael Erps <michaelerps@Michaels-MacBook-Pro.local>
2025-06-23 01:59:32 -0400

    fix(ci): Execute all test commands from plugin root

--- ci.sh
+++ ci.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+# The working directory is /usr/src/app, which is our plugin root.
+
 # Wait for the database to be ready
 wait-for-it.sh db-nonprod:3306 -t 60 -- echo "Database is up"
 
@@ -8,24 +10,17 @@ wait-for-it.sh db-nonprod:3306 -t 60 -- echo "Database is up"
 chmod +x scripts/install-wp-tests.sh
 
 # Use our local, non-interactive script to set up the test environment.
-# This will checkout the WordPress test suite into /tmp/ and our plugin is in /usr/src/app
+# This will create a wp-tests-config.php file in the current directory.
 ./scripts/install-wp-tests.sh wordpress_test root password db-nonprod latest
 
 echo "--- Running PHP Code Sniffer ---"
-# Run from the root, phpcs will find the .phpcs.xml config file automatically
-# and scan ONLY our plugin's files.
-./vendor/bin/phpcs \
-	wp-qr-trackr.php \
-	includes/module-activation.php \
-	includes/module-admin.php \
-	includes/module-ajax.php \
-	includes/module-qr.php \
-	includes/module-requirements.php \
-	includes/module-rewrite.php \
-	includes/module-utils.php
+# Run from the root of our plugin, phpcs will find the .phpcs.xml config file
+# and scan all files in the current directory.
+./vendor/bin/phpcs .
 
 echo "--- Running PHPUnit Tests ---"
-# Run from the root, phpunit will find the phpunit.xml config file automatically.
+# Run from the root, phpunit will find the phpunit.xml and wp-tests-config.php
+# files automatically.
 ./vendor/bin/phpunit
 
 echo "--- Running Stylelint ---"
