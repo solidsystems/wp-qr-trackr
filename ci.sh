77f295eb30d2995117ff88ef8abf53eafd5972de
Michael Erps <michaelerps@Michaels-MacBook-Pro.local>
2025-06-23 01:59:04 -0400

    fix(ci): Precisely target phpcs to plugin files

--- ci.sh
+++ ci.sh
@@ -8,12 +8,21 @@ wait-for-it.sh db-nonprod:3306 -t 60 -- echo "Database is up"
 chmod +x scripts/install-wp-tests.sh
 
 # Use our local, non-interactive script to set up the test environment.
+# This will checkout the WordPress test suite into /tmp/ and our plugin is in /usr/src/app
 ./scripts/install-wp-tests.sh wordpress_test root password db-nonprod latest
 
 echo "--- Running PHP Code Sniffer ---"
 # Run from the root, phpcs will find the .phpcs.xml config file automatically
-# and scan the entire project directory.
-./vendor/bin/phpcs .
+# and scan ONLY our plugin's files.
+./vendor/bin/phpcs \
+	wp-qr-trackr.php \
+	includes/module-activation.php \
+	includes/module-admin.php \
+	includes/module-ajax.php \
+	includes/module-qr.php \
+	includes/module-requirements.php \
+	includes/module-rewrite.php \
+	includes/module-utils.php
 
 echo "--- Running PHPUnit Tests ---"
 # Run from the root, phpunit will find the phpunit.xml config file automatically.
