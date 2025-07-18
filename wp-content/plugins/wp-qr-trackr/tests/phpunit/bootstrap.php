577e90fca44fc030dff439ca338970bfed221dc8
Michael Erps <michael.erps@gmail.com>
2025-07-18 11:09:07 -0400

    tests: fix bootstrap file to load WordPress before calling add_action

--- wp-content/plugins/wp-qr-trackr/tests/phpunit/bootstrap.php
+++ wp-content/plugins/wp-qr-trackr/tests/phpunit/bootstrap.php
@@ -7,16 +7,19 @@
  */
 
 // Load the WordPress test environment.
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
+$_tests_dir = getenv('WP_TESTS_DIR');
+if (!$_tests_dir) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_load_plugin() {
-	require dirname( dirname( dirname( __FILE__ ) ) ) . '/wp-qr-trackr.php';
+function _manually_load_plugin()
+{
+	require dirname(dirname(dirname(__FILE__))) . '/wp-qr-trackr.php';
 }
-add_action( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php'; 
+require $_tests_dir . '/includes/bootstrap.php';
+
+// Load the plugin after WordPress is loaded.
+add_action('muplugins_loaded', '_manually_load_plugin');
